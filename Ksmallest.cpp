68b835aff8fb01cc4378b2e4cb997b2bc7f8e356
rahulsah1999 <54197720+rahulsah1999@users.noreply.github.com>
2019-08-24 10:41:55 +0530

    Add files via upload

---
+++ Ksmallest.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i,j,n,temp;
+	
+	printf("Enter the length of array");
+	cin>>n;
+	
+	int arr[n];
+	
+	printf("Enter the numbers");
+	for(i=0;i<n;i++)
+	cin>>arr[i];
+
+
+
+for(i=0; i<(n-1); i++)
+	{
+		for(j=0; j<(n-i-1); j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+   
+	printf("The sorted array is; \n");
+	
+	for(i=0;i<n;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	cout<<"\n";
+int k=0;
+
+	cout<<"Enter the value of k\n";
+	cin>>k;
+	if (k>=0 && k<=n+1)
+	{
+		cout<<arr[k-1];
+	}
+	else
+	{
+		cout<<"Wrong input";
+	}
+
+ 
+}
